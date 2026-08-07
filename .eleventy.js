667d1bdc5c64491a47c4f9fc2ed8c227858b3cbf
Chiehx0220 <chiehx0220@proton.me>
2026-08-07 15:55:19 +0800

    refactor: flatten bed fields onto room level in CMS

--- .eleventy.js
+++ .eleventy.js
@@ -17,28 +17,38 @@ module.exports = function (eleventyConfig) {
     return (beds || []).filter((bed) => !bed.occupied).map((bed) => bed.label).join("、");
   });
 
+  function roomBeds(room) {
+    const beds = [];
+    let i = 1;
+    while (room[`bed${i}_label`] !== undefined) {
+      beds.push({ label: room[`bed${i}_label`], occupied: !!room[`bed${i}_occupied`] });
+      i++;
+    }
+    return beds;
+  }
+
   eleventyConfig.addFilter("flattenRoomBeds", function (rooms) {
-    return (rooms || []).flatMap((room) => room.beds || []);
+    return (rooms || []).flatMap((room) => roomBeds(room));
   });
 
   eleventyConfig.addFilter("vacantBedLabelsByRoomGender", function (rooms, gender) {
     return (rooms || [])
       .filter((room) => room.gender === gender)
-      .flatMap((room) => (room.beds || []).filter((bed) => !bed.occupied).map((bed) => bed.label))
+      .flatMap((room) => roomBeds(room).filter((bed) => !bed.occupied).map((bed) => bed.label))
       .join("、");
   });
 
   eleventyConfig.addFilter("vacantBedLabelsRoomsNoGenderOccupied", function (rooms) {
     return (rooms || [])
-      .filter((room) => !room.gender && (room.beds || []).some((bed) => bed.occupied))
-      .flatMap((room) => (room.beds || []).filter((bed) => !bed.occupied).map((bed) => bed.label))
+      .filter((room) => !room.gender && roomBeds(room).some((bed) => bed.occupied))
+      .flatMap((room) => roomBeds(room).filter((bed) => !bed.occupied).map((bed) => bed.label))
       .join("、");
   });
 
   eleventyConfig.addFilter("vacantBedLabelsRoomsEmpty", function (rooms) {
     return (rooms || [])
-      .filter((room) => !room.gender && !(room.beds || []).some((bed) => bed.occupied))
-      .flatMap((room) => (room.beds || []).map((bed) => bed.label))
+      .filter((room) => !room.gender && !roomBeds(room).some((bed) => bed.occupied))
+      .flatMap((room) => roomBeds(room).map((bed) => bed.label))
       .join("、");
   });
 
